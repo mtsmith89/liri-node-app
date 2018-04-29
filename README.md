1e499811233744261d56357329ba1c10cbb964e8
Travis Smith <Travis.Smith2@gmail.com>
2018-04-29 17:22:59 -0400

    homework submission

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # liri-node-app
+
+LIRI is knockoff of SIRI
+(currently does not support voice)
+
+## NPM Installations
+
+### Twitter
+
+`npm install twitter`
+
+### Spotify
+
+`npm install node-spotify-api`
+
+### Request
+
+`npm install request`
+
+### FS
+
+`npm install fs`
+
+
+## What can you do...
+
+### my-tweets
+
+Retrieves 20 latest tweets:
+
+`node liri.js my-tweets`"sprint"
+
+### spotify-this-song
+
+Retrieves information for a track:
+
+`node liri.js spotify-this-song "The Sign"`
+
+### movie-this
+
+Retrieves information for a movie:
+
+`node liri.js movie-this "Frozen"`
+
+### do-what-it-says
+
+Gets random text inside a file and does what it says:
+
+`node liri.js do-what-it-says`
+
--- liri.js
+++ liri.js
@@ -1,1 +1,171 @@
 require("dotenv").config();
+
+var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require("fs");
+var keys = require("./keys.js");
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var omdbAPIKEY = Object.values(keys.omdb);
+var liriArg = process.argv[2];
+
+switch (liriArg) {
+  case "my-tweets":
+    myTweets(process.argv[3]);
+    break;
+  case "spotify-this-song":
+    spotifyThisSong(process.argv[3]);
+    break;
+  case "movie-this":
+    movieThis(process.argv[3]);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log(
+      "\r\n" +
+        "Use one of the following commands after 'node liri.js' : " +
+        "\r\n" +
+        "1. my-tweets 'any twitter name' " +
+        "\r\n" +
+        "2. spotify-this-song 'any song name' " +
+        "\r\n" +
+        "3. movie-this 'any movie name' " +
+        "\r\n" +
+        "4. do-what-it-says." +
+        "\r\n"
+    );
+}
+
+function movieThis(movie) {
+  if (!movie) {
+    movie = "mr nobody";
+  }
+  request(
+    "http://www.omdbapi.com/?t=" +
+      movie +
+      "&y=&plot=short&r=json&tomatoes=true&apikey=" + omdbAPIKEY,
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var movieObject = JSON.parse(body);
+        var movieResults =
+        "\n" + "Results:" + "\n\n" +
+        "Title: " +
+          JSON.parse(body).Title +
+          "\n" +
+          "Year: " +
+          JSON.parse(body).Year +
+          "\n" +
+          "Imdb Rating: " +
+          JSON.parse(body).imdbRating +
+          "\n" +
+          "Country: " +
+          JSON.parse(body).Country +
+          "\n" +
+          "Language: " +
+          JSON.parse(body).Language +
+          "\n" +
+          "Plot: " +
+          JSON.parse(body).Plot +
+          "\n" +
+          "Actors: " +
+          JSON.parse(body).Actors +
+          "\n" +
+          "Rotten Tomatoes Rating: " +
+          JSON.parse(body).tomatoRating +
+          "\n" +
+          "Rotten Tomatoes URL: " +
+          JSON.parse(body).tomatoURL +
+          "\n";
+        console.log(movieResults);
+        log(movieResults); 
+      } else {
+        console.log("Error :" + error);
+        return;
+      }
+    }
+  );
+}
+// Tweet function, uses the Twitter module to call the Twitter api
+function myTweets(twitterUsername) {
+  if (!twitterUsername) {
+    twitterUsername = "Smith2Travis";
+  }
+  params = { screen_name: twitterUsername };
+  client.get("statuses/user_timeline/", params, function(
+    error,
+    data,
+    response
+  ) {
+    if (!error) {
+      for (var i = 0; i < data.length; i++) {
+        //console.log(response); // Show the full response in the terminal
+        var twitterResults =
+          "@" +
+          data[i].user.screen_name +
+          ": " +
+          data[i].text +
+          "\r\n" +
+          data[i].created_at +
+          "\r\n" +
+          "------------------------------ " +
+          i +
+          " ------------------------------" +
+          "\r\n";
+        console.log(twitterResults);
+        log(twitterResults); // calling log function
+      }
+    } else {
+      console.log("Error :" + error);
+      return;
+    }
+  });
+}
+
+function spotifyThisSong(songName) {
+    if(!songName){
+        songName = "The sign";
+    }
+    spotify.search({ type: "track", query: songName }, function(err, data) {
+        if(!err){
+            var songInfo = data.tracks.items;
+            for (var i = 0; i < 5; i++) {
+                if (songInfo[i] != undefined) {
+                    var spotifyResults =
+                    "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+                    "Song: " + songInfo[i].name + "\r\n" +
+                    "Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+                    "Album the song is from: " + songInfo[i].album.name + "\r\n" +
+                    "------------------------------ " + i + " ------------------------------" + "\r\n";
+                    console.log(spotifyResults);
+                    log(spotifyResults); // calling log function
+                }
+            }
+        }	else {
+            console.log("Error :"+ err);
+            return;
+        }
+    });
+};
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (!error) {
+      doWhatItSaysResults = data.split(",");
+      console.log(doWhatItSaysResults[1]);
+      spotifyThisSong(doWhatItSaysResults[1]);
+    } else {
+      console.log("Error occurred" + error);
+    }
+  });
+}
+
+function log(logResults) {
+  fs.appendFile("log.txt", logResults, error => {
+    if (error) {
+      throw error;
+    }
+  });
+}
